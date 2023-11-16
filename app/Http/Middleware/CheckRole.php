4dfe4c9c55ef6fd49e23372b60dfc9517071b11f
Billy Joel <billyjoel05sangalang@gmail.com>
2023-11-16 13:48:00 +0800

    Spacing and dockbloc

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -6,6 +6,13 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+    /**
+     * Handle an incoming request.
+     * Checks if the user is logged in or not
+     * If user is logged in check their role
+     * allow user to procced to different parts depending on role
+     */
+
 class CheckRole
 {
     public function handle(Request $request, Closure $next, ...$roles): Response
@@ -14,25 +21,24 @@ class CheckRole
             return redirect(route('login'));
         }
 
-        $userRole = auth()->user()->role;
-
-        
+        $userRole = auth()->user()->role; 
         if (in_array($userRole, ['company', 'supplier']) || ($userRole == 'regular' && !$this->isRestrictedRoute($request))) {
             return $next($request);
         }
 
         return redirect(route('home'))->with('error', 'Unauthorized access');
     }
+      /**
+     * Check if the current route is restricted.
+     * \Illuminate\Http\Request  $request
+     */
 
     protected function isRestrictedRoute(Request $request): bool
-    {
-        
+    {       
         $restrictedRoutes = [
             'farmschedule',
             'coffeeinventory',
         ];
-
-        
         return in_array($request->route()->getName(), $restrictedRoutes);
     }
 }
